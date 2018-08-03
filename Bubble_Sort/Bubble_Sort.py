90e5097a673f4f9f95a40cc057316020041967a3
PuckmanXY <kayoanderson0403@gmail.com>
2018-08-03 10:08:12 -0300

    Some code review and many stupid things avoided

--- Bubble_Sort/Bubble_Sort.py
+++ Bubble_Sort/Bubble_Sort.py
@@ -1,14 +1,10 @@
-def bubbleSort(list):
-  for i in range(len(list) - 1, 0 , -1):
+def bubbleSort(array):
+  size = len(array)
+  for i in range(size - 1, 0 , -1):
     for j in range(i):
-      if list[j] > list[j + 1]:
-        aux = list[j]
-        list[j] = list[j + 1]
-        list[j + 1] = aux
-  return list
+      if array[j] > array[j + 1]:
+        aux = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = aux
+  return array
 
-unordered_list = [23, 42, 8, 16, 15, 4]
-
-ordered_list = bubbleSort(unordered_list)
-
-print(ordered_list)
--- Insertion_Sort/Inserstion_Sort.py
+++ Insertion_Sort/Inserstion_Sort.py
@@ -1,11 +1,12 @@
-def insertionSort(list_to_sort):
-  size = len(list_to_sort)
+def insertionSort(array):
+  size = len(array)
   for i in range(1, size):
-    if list_to_sort[i] < list_to_sort[i - 1]:
-      unsorted_element = list_to_sort[i]
+    if array[i] < array[i - 1]:
+      unsorted_element = array[i]
       j = i
-      while list_to_sort[j - 1] > unsorted_element and j > 0:
-        list_to_sort[j] = list_to_sort[j - 1]
+      while array[j - 1] > unsorted_element and j > 0:
+        array[j] = array[j - 1]
         j = j - 1
-      list_to_sort[j] = unsorted_element
-  print(list_to_sort)
+      array[j] = unsorted_element
+  return array
+  
