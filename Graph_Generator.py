38adfa8df5e444cf8ea27052e92c58b5d8f173e2
PuckmanXY <kayoanderson0403@gmail.com>
2018-08-06 11:42:18 -0300

    A threading approach... Can't get sync return value from calculations yet

--- Graph_Generator.py
+++ Graph_Generator.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import random
 import timeit
+import threading
 
 from Selection_Sort.SelectionSort import selectionSort
 ## Edit line above to include the desired sorting function
@@ -8,13 +9,28 @@ from Selection_Sort.SelectionSort import selectionSort
 ## has a __init__.py, so python understands it as a package
 
 
- def drawGraphic(x, y, z, xl="Entradas", yl="Saídas"):
-     plt.plot(x, y, label="Caso médio")
-     plt.plot(x, z, label="Pior caso")
-     plt.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
-     plt.ylabel(yl)
-     plt.xlabel(xl)
-     plt.show()
+#  def drawGraphic(x, y, z, xl="Entradas", yl="Saídas"):
+#      plt.plot(x, y, label="Caso médio")
+#      plt.plot(x, z, label="Pior caso")
+#      plt.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
+#      plt.ylabel(yl)
+#      plt.xlabel(xl)
+#      plt.show()
+
+class CalculationThread (threading.Thread):
+    def __init__(self, threadID, name, sorting_method, list_generating_method, numbers_to_generate):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.sorting_method = sorting_method
+        self.list_generating_method = list_generating_method
+        self.numbers_to_generate = numbers_to_generate
+        self.results = []
+
+    def run(self):
+        time_to_generate = [timeToSort(self.sorting_method, self.list_generating_method, size) for size in self.numbers_to_generate]
+        return time_to_generate
+        
 
 def generateRandomList(size):
     random_list = []
@@ -33,11 +49,11 @@ def timeToSort(sorting_method, list_generating_method, list_size):
     list_to_sort = list_generating_method(list_size)
     print('Calculando para ' + str(list_size))
     time = timeit.timeit(sorting_method + "({})".format(list_to_sort), setup="from __main__ import " + sorting_method,
-                         number=1)
+                        number=1)
     print('Tempo necessário: ' + str(time))
 
     return time
-        
+
 if __name__ == '__main__':
     ## Edit value of sorting_method so it can be correctly passed
     ## as argument to timeToSort function
@@ -45,8 +61,10 @@ if __name__ == '__main__':
     ## function name in its package
     sorting_method = "selectionSort"
     numbers_to_generate = [1000, 10000, 20000, 30000, 40000, 50000, 100000]
-    time_to_generate_medium_case = [timeToSort(sorting_method, generateRandomList, size) for size in numbers_to_generate]
-    time_to_generate_worst_case = [timeToSort(sorting_method, generateDecreasingList, size) for size in numbers_to_generate]
-
-    drawGraphic(numbers_to_generate, time_to_generate_medium_case, time_to_generate_worst_case, "Tamanho do array para ordenar", "Tempo")
+    medium_case_thread = CalculationThread(1, 'Medium Case', sorting_method, generateRandomList, numbers_to_generate)
+    worst_case_thread = CalculationThread(2, 'Worst Case', sorting_method, generateRandomList, numbers_to_generate)
+    
+    variable = medium_case_thread.start()
+    worst_case_thread.start()
+    #drawGraphic(numbers_to_generate, time_to_generate_medium_case, time_to_generate_worst_case, "Tamanho do array para ordenar", "Tempo")
 
